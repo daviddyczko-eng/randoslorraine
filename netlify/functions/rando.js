a734651127075d3d2e367be89fab944f44b180ef
daviddyczko-eng <david.dyczko@laposte.net>
2026-07-02 10:22:01 +0200

    Update regex to match new rando URL pattern

--- netlify/functions/rando.js
+++ netlify/functions/rando.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
     const home = await homeResponse.text();
 
     // 2) Trouver l'URL de la prochaine rando
-    const matchUrl = home.match(/href="(\/\d{4}-\d{2}-\d{2}[^"]+)"/);
+    const matchUrl = home.match(/href="(\/\d{4}-\d{2}-\d{2}-rando[^"]+)"/);
 
     if (!matchUrl) {
       return {
