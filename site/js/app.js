58027db4b1cffaeaa8a497a4ab1b5b6d742865e1
daviddyczko-eng <david.dyczko@laposte.net>
2026-09-01 08:17:59 +0200

    Remove unused variables from app.js

--- site/js/app.js
+++ site/js/app.js
@@ -185,8 +185,6 @@ let prochaineRando = null;
 
 const covoiturage = "+33672997397";
 const tresorerie = "+33689928560";
-let places = 1;
-let etape = "";
 let infoContent = null;
 let backHandler = null;
 let participantsWarningDisabled =
