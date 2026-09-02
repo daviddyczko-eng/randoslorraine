2ac9bfe189998d96f037ecffdec4c386103080fa
daviddyczko-eng <david.dyczko@laposte.net>
2026-09-02 23:27:09 +0200

    Change SVG size for QR scan button
    Updated the SVG dimensions for the QR scan button.

--- site/js/app.js
+++ site/js/app.js
@@ -4072,7 +4072,7 @@ function renderParticipants() {
                         class="qr-scan-button"
                         id="btn-scan-pilote"
                         title="Scanner le QR code du pilote">
-                        <svg width="130" height="130" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="200" height="200" viewBox="30 30 250 250" xmlns="http://www.w3.org/2000/svg">
   <!-- Carrés de positionnement (coins) -->
   <!-- Coin supérieur gauche -->
   <rect x="10" y="10" width="70" height="70" />
