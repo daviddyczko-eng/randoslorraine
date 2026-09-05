0b92ce1e0e57effc15df8fe050a0a04dc6ba1dc4
daviddyczko-eng <david.dyczko@laposte.net>
2026-09-05 10:22:09 +0200

    Change button SVG fill color to currentColor

--- site/js/app.js
+++ site/js/app.js
@@ -913,12 +913,12 @@ function renderRandoDetails(
                                     onclick="window.open('${mapsUrl}','_blank')"
                                     title="Ouvrir dans Google Maps">
 <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-  <rect width="32" height="32" rx="7" fill=var(--brand)/>
+  <rect width="32" height="32" rx="7" fill="currentColor"/>
   <text x="50%" y="50%" 
         font-family="Arial, Helvetica, sans-serif" 
         font-weight="700" 
         font-size="17" 
-        fill=var(--brand-pale)
+        fill="--brand-pale"
         text-anchor="middle" 
         dominant-baseline="central">P</text>
 </svg>
