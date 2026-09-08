32fc469ab8f88a56c65d3fa4cea73e1e515bc335
daviddyczko-eng <david.dyczko@laposte.net>
2026-09-08 13:47:40 +0200

    Refactor service worker registration code
    Refactor service worker registration for better readability.

--- site/js/app.js
+++ site/js/app.js
@@ -10,19 +10,21 @@ import {
     qrData
 } from "./storage.js";
 /* ============================================================
- * Désactivation du Service Worker (développement)
+ * Service Worker
  * ============================================================
  */
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js") // Chemin absolu
-            .then(() => console.log("✅ Service Worker enregistré"))
-            .catch((error) => console.error("❌ Erreur Service Worker :", error));
-    });
-}
-if ("caches" in window) {
-    caches.keys().then(keys => {
-        keys.forEach(key => caches.delete(key));
+        navigator.serviceWorker.register("/sw.js")
+            .then(() =>
+                console.log("✅ Service Worker enregistré")
+            )
+            .catch((error) =>
+                console.error(
+                    "❌ Erreur Service Worker :",
+                    error
+                )
+            );
     });
 }
 
